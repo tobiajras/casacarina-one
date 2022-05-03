77dd7369165308fd7855487081182782a355e06e
Tobias <tobyajras@gmail.com>
2022-05-03 14:40:55 -0300

    link fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,28 +1,26 @@
-import { Link } from "react-router-dom";
-
 export const Footer = () => {
   return (
-    <div className="footer__divParent">
-      <div className="footer__firstDiv">
+    <div className='footer__divParent'>
+      <div className='footer__firstDiv'>
         <i></i>
-        <a href="">7500-8000</a>
+        <a href=''>7500-8000</a>
         <i></i>
-        <a href="">-8000</a>
+        <a href=''>-8000</a>
       </div>
-      <div className="footer__secondDiv">
-        <div className="footer__childSecondDiv">
+      <div className='footer__secondDiv'>
+        <div className='footer__childSecondDiv'>
           <img
-            className="footer__img"
-            src="/assets/logoCasaCarina2.svg"
-            alt="logoCasaCarina2"
+            className='footer__img'
+            src='/assets/logoCasaCarina2.svg'
+            alt='logoCasaCarina2'
           />
         </div>
       </div>
-      <div className="footer__thirdtDiv">
-        <i class="fas fa-times"></i>
-        <i class="fa-brands fa-whatsapp-square"></i>
-        <i class="fa-brands fa-facebook-square"></i>
-        <i class="fa-brands fa-instagram-square"></i>
+      <div className='footer__thirdtDiv'>
+        <i class='fas fa-times'></i>
+        <i class='fa-brands fa-whatsapp-square'></i>
+        <i class='fa-brands fa-facebook-square'></i>
+        <i class='fa-brands fa-instagram-square'></i>
       </div>
     </div>
   );
--- src/pages/home/HomeScreen.js
+++ src/pages/home/HomeScreen.js
@@ -1,21 +1,21 @@
-import React from "react";
-import AutoCarousel from "../../components/AutoCarousel/AutoCarousel";
+import React from 'react';
+import AutoCarousel from '../../components/AutoCarousel/AutoCarousel';
 // import { Carousel } from "../../components/Carousel/Carousel";
-import CarouselSwiper from "../../components/CarouselSwiper/CarouselSwiper";
-import { Footer } from "../../components/Footer/Footer";
-import { Informacion } from "../../components/Informacion/Informacion";
-import { Categories } from "../products/Categories";
+import CarouselSwiper from '../../components/CarouselSwiper/CarouselSwiper';
+import { Footer } from '../../components/Footer/Footer';
+import { Informacion } from '../../components/Informacion/Informacion';
+import { Categories } from '../products/Categories';
 
 export const HomeScreen = () => {
   return (
     <div>
       {/* <Carousel2 /> */}
       <CarouselSwiper />
-      <h1 className="homescreen__title">Productos</h1>
+      <h1 className='homescreen__title'>Productos</h1>
       <Categories />
-      <h1 className="homescreen__title">Marcas</h1>
+      <h1 className='homescreen__title'>Marcas</h1>
       <AutoCarousel />
-      <h1 className="homescreen__title">Información</h1>
+      <h1 className='homescreen__title'>Información</h1>
       <Informacion />
       {/* <Footer /> */}
     </div>
