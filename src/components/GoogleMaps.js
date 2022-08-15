aec332c3c21f5526c0fc73908a32b9b60bf2cd28
Tobias <tobyajras@gmail.com>
2022-08-15 02:06:57 -0300

    inframe title

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -2,6 +2,7 @@ export const GoogleMaps = () => {
   return (
     <div className='googleMaps__div'>
       <iframe
+        title='googleMaps'
         className='googleMaps__iframe'
         src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3278.6404915688795!2d-58.46148638423345!3d-34.73945527220095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccde09e8f565d%3A0xda0c8e4831dd87e0!2sCasa%20Carina!5e0!3m2!1ses!2sar!4v1660532775602!5m2!1ses!2sar'
         width='600'
