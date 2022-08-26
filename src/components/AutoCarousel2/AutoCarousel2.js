10f270f3692551eb879ee2eca79c22b5a88163bf
Tobias <tobyajras@gmail.com>
2022-08-25 23:19:44 -0300

    brands carousel changed

--- src/components/AutoCarousel2/AutoCarousel2.js
+++ src/components/AutoCarousel2/AutoCarousel2.js
@@ -1,21 +1,74 @@
-import React from 'react';
-
-import './autoCarousel2.scss';
-import { marcasData } from '../../data/marcasData';
+import './carouselCss.css';
 
 export const AutoCarousel2 = () => {
   return (
-    <div
-      className='autoCarousel2__parentDiv'
-      onAnimationIterationCapture={console.log('itero!')}
-    >
-      {marcasData.map((marca) => {
-        return (
-          <div key={marca.id} className='autoCarousel2__div'>
-            <img src={`/assets/Marcas/${marca.src}`} alt='' />
+    <div className='wrapper'>
+      <div className='slider'>
+        <div className='slide'>
+          <div className='slideImg'>
+            <img src='/assets/marcas/LomanegraLogo.png' alt='imgSlider' />
           </div>
-        );
-      })}
+          <div className='slideImg'>
+            <img src='/assets/marcas/WeberLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/AcindarLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/LourdesLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/TersuaveLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/NovoplackLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/KlaukolLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/EternitLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/CañuelasLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/CannonLogo.png' alt='imgSlider' />
+          </div>
+        </div>
+        <div className='slide'>
+          <div className='slideImg'>
+            <img src='/assets/marcas/LomanegraLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/WeberLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/AcindarLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/LourdesLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/TersuaveLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/NovoplackLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/KlaukolLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/EternitLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/CañuelasLogo.png' alt='imgSlider' />
+          </div>
+          <div className='slideImg'>
+            <img src='/assets/marcas/CannonLogo.png' alt='imgSlider' />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/home/HomeScreen.js
+++ src/pages/home/HomeScreen.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import AutoCarousel from '../../components/AutoCarousel/AutoCarousel';
 import { AutoCarousel2 } from '../../components/AutoCarousel2/AutoCarousel2';
 import CarouselSwiper from '../../components/CarouselSwiper/CarouselSwiper';
 import { GoogleMaps } from '../../components/GoogleMaps';
@@ -13,9 +12,8 @@ export const HomeScreen = () => {
       <h1 className='homescreen__title'>Productos</h1>
       <Categories />
       <h1 className='homescreen__title'>Marcas</h1>
-      <AutoCarousel />
-      <h1 className='homescreen__title'>Información</h1>
       <AutoCarousel2 />
+      <h1 className='homescreen__title'>Información</h1>
       <Informacion />
       <h1 className='homescreen__title'>Ubicación</h1>
       <GoogleMaps />
