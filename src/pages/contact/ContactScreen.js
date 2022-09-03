6fd2e12313a968b30178915f8f8211103fc2fa5e
Tobias <tobyajras@gmail.com>
2022-09-03 20:29:01 -0300

    categories with absolute title

--- src/pages/contact/ContactScreen.js
+++ src/pages/contact/ContactScreen.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { WhatsappBanner } from '../../components/WhatsappBanner';
 import { GoogleMaps } from '../../components/GoogleMaps';
 import { Horarios } from '../../components/Informacion/Horarios';
+import { Tarjetas } from '../../components/Informacion/Tarjetas';
+import { Envios } from '../../components/Informacion/Envios';
+import { Atencion } from '../../components/Informacion/Atencion';
 
 export const ContactScreen = () => {
   return (
     <div>
       <WhatsappBanner />
-      <Horarios />
+      <div className='homescreen__informacion'>
+        <Horarios />
+        <Tarjetas />
+        <Envios />
+        <Atencion />
+      </div>
       <GoogleMaps />
     </div>
   );
