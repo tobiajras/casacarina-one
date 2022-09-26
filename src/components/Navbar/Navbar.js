6760573a86c3b9f7b3420e151cd3c6c0c34c9eff
Tobias <tobyajras@gmail.com>
2022-09-25 22:56:39 -0300

    center nav max-width

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,62 +9,66 @@ export const Navbar = () => {
   };
 
   return (
-    <div className='navbar__parentDiv'>
-      <div className='navbar__childDiv'>
-        <Link to={'/'} className='navbar__logoLink'>
-          <img src='/assets/logoCasaCarina.svg' alt='LogoCasaCarina' />
-        </Link>
+    <header className='navbar__header'>
+      <div className='navbar__parentDiv'>
+        <div className='navbar__childDiv'>
+          <Link to={'/'} className='navbar__logoLink'>
+            <img src='/assets/logoCasaCarina.svg' alt='LogoCasaCarina' />
+          </Link>
+        </div>
+        <div className='navbar__menuIcon' onClick={handleClickMenu}>
+          <i className={clicked ? 'fas fa-times' : 'fa fa-bars'}></i>
+        </div>
+        <nav
+          className={clicked ? 'navbar__menuList' : 'navbar__menuList close'}
+        >
+          <div></div>
+          <NavLink
+            onClick={handleClickMenu}
+            className={(navData) =>
+              navData.isActive
+                ? 'navbar__childNavlink_active'
+                : 'navbar__childNavlink'
+            }
+            to={'/'}
+          >
+            Inicio
+          </NavLink>
+          <NavLink
+            onClick={handleClickMenu}
+            className={(navData) =>
+              navData.isActive
+                ? 'navbar__childNavlink_active'
+                : 'navbar__childNavlink'
+            }
+            to={'productos'}
+          >
+            Productos
+          </NavLink>
+          <NavLink
+            onClick={handleClickMenu}
+            className={(navData) =>
+              navData.isActive
+                ? 'navbar__childNavlink_active'
+                : 'navbar__childNavlink'
+            }
+            to={'nosotros'}
+          >
+            Nosotros{' '}
+          </NavLink>
+          <NavLink
+            onClick={handleClickMenu}
+            className={(navData) =>
+              navData.isActive
+                ? 'navbar__childNavlink_active'
+                : 'navbar__childNavlink'
+            }
+            to={'contacto'}
+          >
+            Contacto
+          </NavLink>
+        </nav>
       </div>
-      <div className='navbar__menuIcon' onClick={handleClickMenu}>
-        <i className={clicked ? 'fas fa-times' : 'fa fa-bars'}></i>
-      </div>
-      <nav className={clicked ? 'navbar__menuList' : 'navbar__menuList close'}>
-        <div></div>
-        <NavLink
-          onClick={handleClickMenu}
-          className={(navData) =>
-            navData.isActive
-              ? 'navbar__childNavlink_active'
-              : 'navbar__childNavlink'
-          }
-          to={'/'}
-        >
-          Inicio
-        </NavLink>
-        <NavLink
-          onClick={handleClickMenu}
-          className={(navData) =>
-            navData.isActive
-              ? 'navbar__childNavlink_active'
-              : 'navbar__childNavlink'
-          }
-          to={'productos'}
-        >
-          Productos
-        </NavLink>
-        <NavLink
-          onClick={handleClickMenu}
-          className={(navData) =>
-            navData.isActive
-              ? 'navbar__childNavlink_active'
-              : 'navbar__childNavlink'
-          }
-          to={'nosotros'}
-        >
-          Nosotros{' '}
-        </NavLink>
-        <NavLink
-          onClick={handleClickMenu}
-          className={(navData) =>
-            navData.isActive
-              ? 'navbar__childNavlink_active'
-              : 'navbar__childNavlink'
-          }
-          to={'contacto'}
-        >
-          Contacto
-        </NavLink>
-      </nav>
-    </div>
+    </header>
   );
 };
