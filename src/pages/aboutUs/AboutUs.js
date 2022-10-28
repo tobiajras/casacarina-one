20f4c53c0093b025341579817c2cd3ca5fd9e43b
Tobias <tobyajras@gmail.com>
2022-10-28 16:14:17 -0300

    center

--- src/pages/aboutUs/AboutUs.js
+++ src/pages/aboutUs/AboutUs.js
@@ -6,21 +6,26 @@ export const AboutUs = () => {
   return (
     <div>
       <VisitarBanner />
-      <div className='aboutUs__divDescripcion'>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-          doloribus voluptatibus quae dolore, exercitationem recusandae officiis
-          iste molestias dignissimos molestiae libero maxime voluptates neque
-          autem eligendi velit laudantium ratione harum modi excepturi.
-        </p>
-        <p>
-          Exercitationem recusandae officiis iste molestias dignissimos
-          molestiae libero maxime voluptates neque autem eligendi velit
-          laudantium ratione harum modi excepturi.
-        </p>
+      <div className='aboutUs__divDescripcionFlex'>
+        <div className='aboutUs__divDescripcion'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            doloribus voluptatibus quae dolore, exercitationem recusandae
+            officiis iste molestias dignissimos molestiae libero maxime
+            voluptates neque autem eligendi velit laudantium ratione harum modi
+            excepturi.
+          </p>
+          <p>
+            Exercitationem recusandae officiis iste molestias dignissimos
+            molestiae libero maxime voluptates neque autem eligendi velit
+            laudantium ratione harum modi excepturi.
+          </p>
+        </div>
       </div>
-      <div className='aboutUs__GaleriaImagenes'>
-        <GaleriaImagenes />
+      <div className='aboutUs__GaleriaImagenesFlex'>
+        <div className='aboutUs__GaleriaImagenes'>
+          <GaleriaImagenes />
+        </div>
       </div>
     </div>
   );
